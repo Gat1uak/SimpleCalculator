1ad07bfe7062b7548dbc044c8fbad4c173ee92c0
Gatluak Tholbok <tholbok@Gatluaks-MacBook-Pro.local>
2019-12-26 20:46:28 +1100

    Completed application fixed all bugs

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Connection made!");
+
+const btns = document.querySelectorAll('.btn');
+const screen = document.querySelector('.screen');
+const equalBtn = document.querySelector('.btn-equal');
+const clearBtn = document.querySelector('.btn-clear');
+
+for (let i = 0; i < btns.length; i++) {
+   btns[i].addEventListener('click', function() {
+      let number = btns[i].getAttribute('data-num');
+      screen.value += number;
+   });
+}
+equalBtn.addEventListener('click', function () {
+   let sum = eval(screen.value);
+   screen.value = sum;
+});
+
+clearBtn.addEventListener('click', function() {
+   screen.value = '';
+})
